43daaa8078527a92438a51885bb40f6af80560c7
tonkaxxx <maltsev.yar@gmail.com>
2025-08-02 11:13:15 +0300

    fresh arch

---
+++ Code - OSS/User/History/-704e947d/SyMW.dockerfile
@@ -0,0 +1,3 @@
+apk add 'kubelet=~1.33'
+apk add 'kubeadm=~1.33'
+apk add 'kubectl=~1.33'
--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,15 @@
 if status is-interactive
     abbr -a gerproxy "ssh -D 1080 -N -C root@168.119.168.160"
-    # Commands to run in interactive sessions can go here
+    
+    abbr -a ga "git add"
+    abbr -a gs "git status"
+    abbr -a gc "git commit -m"
+    abbr -a gps "git push"
+    abbr -a gpl "git pull"
+
+    abbr -a dcs "docker container stop"
+
+    abbr -a k "kubectl"
+
+    abbr -a auto "/home/user/.config/autostart"
 end
